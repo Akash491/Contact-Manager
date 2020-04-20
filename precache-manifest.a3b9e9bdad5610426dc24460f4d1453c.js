a6ad37e088f96139ef1675a469ee2e7d6e80969d
Akash Agarwal <33690484+Akash491@users.noreply.github.com>
2020-04-20 15:49:48 +0530

    Updates

---
+++ precache-manifest.a3b9e9bdad5610426dc24460f4d1453c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5553c3a8a71fac12cceb47632f61032b",
+    "url": "/Contact-Manager/index.html"
+  },
+  {
+    "revision": "4d76730f93b9c82bf33a",
+    "url": "/Contact-Manager/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "caa873540540c5bac1fa",
+    "url": "/Contact-Manager/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "4d76730f93b9c82bf33a",
+    "url": "/Contact-Manager/static/js/2.8625a6ea.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Contact-Manager/static/js/2.8625a6ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caa873540540c5bac1fa",
+    "url": "/Contact-Manager/static/js/main.e0b0ea9f.chunk.js"
+  },
+  {
+    "revision": "a561b56380079e4af95c",
+    "url": "/Contact-Manager/static/js/runtime-main.a8e7ecf7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Contact-Manager/static/media/logo.5d5d9eef.svg"
+  }
+]);
